3d007a0269582b018bfbb1dc17f046c843efd32e
codeDevLogan <logandevscode@gmail.com>
2023-01-03 13:26:28 -0700

    Added the function for censor case.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
 let str = document.querySelector("#username");
 
-let stringToLowerCase = function(str){
+let stringToLowerCase = function(){
     return str.toLowerCase();
+}
+
+let stringToSarcasm = function(){
+    for(let i = 1; i<str.length; i++){
+        str[i] = "*";
+    }
+    return str;
 }
